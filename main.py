9bce00af1204e566530ce5e13eb4cddab02001cc
PlanetEditorX <962129122@qq.com>
2024-09-26 12:56:00 +0800

    读取视频ISO格式日期

--- main.py
+++ main.py
@@ -48,7 +48,13 @@ def get_exif_data(path, type = 0):
             for track in media_info.tracks:
                 if track.track_type in ['General', 'Video']:
                     # 尝试获取编码日期，这可能与拍摄日期相关
-                    encoded_date = getattr(track, 'encoded_date', None)
+                    encoded_date = getattr(track, 'comapplequicktimecreationdate', None)
+                    if encoded_date:
+                        print(f"视频文件 {path} 读取到的拍摄日期为: {encoded_date}")
+                        # 解析ISO格式的日期时间字符串
+                        return datetime.fromisoformat(encoded_date)
+                    if not encoded_date:
+                        encoded_date = getattr(track, 'encoded_date', None)
                     if not encoded_date:
                         encoded_date = getattr(track, 'tagged_date', None)
                     if encoded_date:
